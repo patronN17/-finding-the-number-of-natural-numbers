f4f528f16b502bbd139b4dce2528d6a330cf3e67
Patron <Patron@LAPTOP-VIEAF2J0>
2023-01-12 02:29:54 +0300

    Добавьте файлы проекта.

---
+++ пр4 з 7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace пр4_з_7
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+
+                int number, i = 1, a = 0, result = 0;
+                Console.WriteLine("Введите количество чисел в сумме: "); 
+                number = Convert.ToInt32(Console.ReadLine()); 
+                Console.WriteLine("\nЧисла, удовлетворяющие условиям задачи: "); 
+                while (i <= number)
+                {   
+                    a = i;            
+                    i++;
+                    if (a % 5 == 2 || a % 3 == 1)
+                    { 
+                      
+                        result += a;         
+                        Console.Write(a + ", "); 
+                    }
+                }
+                Console.WriteLine("\n"); 
+
+                Console.WriteLine("Сумма выведенных чисел = " + result); 
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка! Надо ввести натуральное число!");
+            }
+        }
+    }
+}
